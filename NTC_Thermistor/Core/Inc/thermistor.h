d5cdb9aa48f06288aee80703b0c8a3f44c9ca3e0
Leslie Dong <leslied4@uci.edu>
2025-12-02 02:21:29 -0800

    "working code for one therm -- still needs calibration"

---
+++ NTC_Thermistor/Core/Inc/thermistor.h
@@ -0,0 +1,30 @@
+///*
+// * thermistor.h
+// *
+// *  Created on: Oct 23, 2025
+// *      Author: leslie
+// */
+
+
+#ifndef INC_THERMISTOR_H_
+#define INC_THERMISTOR_H_
+
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Declare a C-callable function
+float readThermistor(void);
+uint32_t read_ADC_value(void);
+float ntc_convertToC(uint32_t adcValue);
+
+
+#ifdef __cplusplus
+}
+
+#endif
+
+#endif /* INC_THERMISTOR_H_ */
---
+++ NTC_Thermistor/Core/Src/thermistor.cpp
@@ -0,0 +1,59 @@
+
+#include "thermistor.h"
+#include "main.h"
+#include <math.h>
+
+
+#define _NTC_R_SERIES         10000.0f
+#define _NTC_R_NOMINAL        10000.0f
+#define _NTC_TEMP_NOMINAL     25.0f
+#define _NTC_ADC_MAX          65535 //  16bit
+#define _NTC_BETA             3950
+
+extern ADC_HandleTypeDef hadc1;
+
+// readThermistor calls the other two functions in this file
+// gets the ADC_value and converts it to temp in Celsius, returns temp
+float readThermistor(void){
+	uint32_t adcValue = read_ADC_value();
+	float tempC = ntc_convertToC(adcValue);
+	return tempC;
+}
+
+
+// ----------------------------------------------------
+// Reads ADC value from configured channel
+// ----------------------------------------------------
+uint32_t read_ADC_value(void){
+	uint32_t adcValue = 0;
+
+	HAL_ADC_Start(&hadc1); 									// Start ADC conversion
+	if(HAL_ADC_PollForConversion(&hadc1, 100) == HAL_OK)
+	  {
+		  adcValue = HAL_ADC_GetValue(&hadc1);
+	  }
+	HAL_ADC_Stop(&hadc1);
+	return adcValue;
+
+}
+
+// ----------------------------------------------------
+// Converts ADC value → Celsius
+// ----------------------------------------------------
+float ntc_convertToC(uint32_t adcValue)
+{
+
+	if (adcValue == 0) adcValue = 1;
+	if (adcValue >= _NTC_ADC_MAX) adcValue = _NTC_ADC_MAX - 1;
+
+	float rntc = (float)_NTC_R_SERIES / (((float)_NTC_ADC_MAX / (float)adcValue ) - 1.0f);
+	float temp;
+	temp = rntc / (float)_NTC_R_NOMINAL;
+	temp = logf(temp);
+	temp /= (float)_NTC_BETA;
+	temp += 1.0f / ((float)_NTC_TEMP_NOMINAL + 273.15f);
+	temp = 1.0f / temp;
+	temp -= 273.15f;
+	return temp;
+}
+
